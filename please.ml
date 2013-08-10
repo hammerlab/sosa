f5c1e16e873eea5d3800239ce5e02a2529a6eaf2
Sebastien Mondet <seb@mondet.org>
2013-08-10 12:21:39 -0400

    build: update generated files

--- please.ml
+++ please.ml
@@ -1,7 +1,7 @@
 #! /usr/bin/env ocaml
  open Printf
  module List = ListLabels
- let say fmt = ksprintf (printf "please.ml-> %s\n%!") fmt
+ let say fmt = ksprintf (printf "Please-> %s\n%!") fmt
  let cmdf fmt =
    ksprintf (fun s -> ignore (Sys.command s)) fmt
  let chain l =
@@ -30,11 +30,11 @@ let build () =
       chain [
         "pwd";
         "cp ../sosa.ml .";
-        "ocamlfind ocamlc  -c sosa.ml -annot -o sosa.cmo";
-        "ocamlfind ocamlopt  -c sosa.ml -annot -o sosa.cmx";
+        "ocamlfind ocamlc  -c sosa.ml -o sosa.cmo";
+        "ocamlfind ocamlopt  -c sosa.ml  -annot -bin-annot -o sosa.cmx";
         "ocamlc sosa.cmo -a -o sosa.cma";
         "ocamlopt sosa.cmx -a -o sosa.cmxa";
-        "ocamlopt sosa.cmx -shared -o sosa.cmxs";
+        "ocamlopt sosa.cmxa sosa.a -shared -o sosa.cmxs";
       ];
     )
 
@@ -47,7 +47,8 @@ let install () =
 let build_doc () =
     in_build_directory (fun () ->
         chain [
-          sprintf "ocamlfind ocamldoc  -charset UTF-8 -colorize-code -html sosa.ml";
+          "mkdir -p doc";
+                       sprintf "ocamlfind ocamldoc  -charset UTF-8 -keep-code -colorize-code -html sosa.ml -d doc/";
         ])
 
 let name = "sosa"
@@ -70,6 +71,9 @@ let () =
   | _ :: "clean" :: []
   | _ :: "C" :: [] ->
     cmdf "rm -fr _build"
+  | _ :: "help" :: [] ->
+    say "usage: ocaml %s [build|build_doc|install|uninstall|clean]" Sys.argv.(0);
   | _ ->
-    say "usage: ocaml %s [build|build_doc|install|uninstall|clean]" Sys.argv.(0)
-
+    say "usage: ocaml %s [build|build_doc|install|uninstall|clean]" Sys.argv.(0);
+    exit 1
+    
