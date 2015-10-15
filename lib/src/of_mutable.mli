1ac5ef406ced4a2ede78d3d49fc90dcc8fe62618
Sebastien Mondet <seb@mondet.org>
2015-10-15 15:47:01 -0400

    Export types in `List_of` and `Of_mutable`

--- lib/src/of_mutable.mli
+++ lib/src/of_mutable.mli
@@ -2,4 +2,5 @@
    {!modtype:Api.MINIMALISTIC_MUTABLE_STRING} to build
    {!modtype:Api.BASIC_STRING}. *)
 
-module Make (S : Api.MINIMALISTIC_MUTABLE_STRING) : Api.BASIC_STRING
+module Make (S : Api.MINIMALISTIC_MUTABLE_STRING) :
+  Api.BASIC_STRING with type character = S.character
